a64cffa723e66e8bf14f319b4c3d6ab7a305c57f
brunooselladev <brunooselladev@gmail.com>
2025-10-16 12:39:09 -0300

    A VER

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
-import { componentTagger } from 'lovable-tagger'
 
-export default defineConfig(({ mode }) => ({
-  base: '/', // 👈 NECESARIO para producción
+export default defineConfig({
+  base: './',
   build: {
-    outDir: 'dist', // 👈 Vercel servirá desde aquí
+    outDir: 'dist',
   },
-  server: {
-    host: '::',
-    port: 8080,
-    proxy:
-      mode === 'development'
-        ? {
-            '/api': {
-              target: 'http://localhost:3001',
-              changeOrigin: true,
-              secure: false,
-            },
-          }
-        : undefined,
-  },
-  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      '@': path.resolve(process.cwd(), './src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-}))
+})
